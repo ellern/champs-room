0c80c587ffc0caa2f1ef1e600dccc4fd4f881db9
ellern <martin.ellern.bilgrau@gmail.com>
2013-11-28 12:20:22 +0100

    Display leagues on frontpage

--- ChampsRoom/Controllers/HomeController.cs
+++ ChampsRoom/Controllers/HomeController.cs
@@ -1,13 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using ChampsRoom.Helpers;
+using ChampsRoom.Models;
+using ChampsRoom.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace ChampsRoom.Controllers
 {
     public class HomeController : Controller
     {
+        private DataContext db = new DataContext();
+
         [Route("")]
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var leagues = await db.Leagues.ToListAsync();
+
+            return View(leagues);
         }
     }
 }
