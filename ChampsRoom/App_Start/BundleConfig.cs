7e25abb6463c7aed5f084d0061571378a3a5cc6e
ellern <martin.ellern.bilgrau@gmail.com>
2014-09-04 23:58:45 +0200

    Changed to use cdn for js/css libs

--- ChampsRoom/App_Start/BundleConfig.cs
+++ ChampsRoom/App_Start/BundleConfig.cs
@@ -9,16 +9,9 @@ namespace ChampsRoom
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/scripts/js").Include(
-                "~/Scripts/jquery-{version}.js",
-                "~/Scripts/jquery.unobtrusive*",
-                "~/Scripts/jquery.validate*",
-                "~/Scripts/knockout-{version}.js",
-                "~/Scripts/bootstrap.js",
                 "~/Scripts/application.js"));
 
             bundles.Add(new StyleBundle("~/content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/font-awesome.css",
                 "~/Content/application.css"));
         }
     }
